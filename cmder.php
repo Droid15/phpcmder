ba8ff34a0e72f8c47ca48616e990cf2dc3ca7403
Droid15 <lyr1234@126.com>
2024-03-26 17:01:07 +0800

    Update cmder.php
    Implement using the readline function

--- cmder.php
+++ cmder.php
@@ -2,32 +2,35 @@
 
 fflush(STDIN);
 echo "\n\e[33m ############ 欢迎来到phpcmder v0.1! 需要帮助请输入help ###########\n\n";
-echo "\e[0m >>> ";
+echo "\e[0m";
 while(1){
-   $cmd = fgets(STDIN);
-   $cmd = trim($cmd);
-   if($cmd=='exit'||$cmd=='exit()'){
+    if (!function_exists("readline")) {
+        echo "\e[33m Readline extension not available.\n";
+        break;
+    }
+
+    readline_completion_function(function ($cmd) {
+        return [];
+    });
+
+    $cmd = readline(">>> ");
+    $cmd = trim($cmd);
+    if($cmd=='exit'||$cmd=='exit()'){
        echo "bye!\n";
        break;
-   }
-   if($cmd=='help'){
-     help(); continue;
-   }
+    }
+    if($cmd=='help'){
+       help(); continue;
+    }
 
-   $arr_179633571=[];
-   $os = strtolower( php_uname('s') );
-   if($os=='windows nt'){
-      exec('php -r "'.$cmd.'"',$arr_179633571);
-   }else{
-      exec("php -r '$cmd'",$arr_179633571);
-   }
-
-   if(count($arr_179633571)==1){
-      echo $arr_179633571[0]."\n";
-   }else{
-      echo json_encode(array_values($arr_179633571), JSON_UNESCAPED_UNICODE+JSON_PRETTY_PRINT)."\n";
-   }
-   echo '>>> ';
+    $arr_179633571=[];
+    $os = strtolower( php_uname('s') );
+    if($os=='windows nt'){
+        exec('php -r "'.$cmd.'"',$arr_179633571);
+    }else{
+        exec("php -r '$cmd'",$arr_179633571);
+    }
+    var_dump($arr_179633571);
 }
 
 function help(){
