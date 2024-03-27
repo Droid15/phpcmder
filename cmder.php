f05cd2902f250dbe084836cc7b55a6ffb2cd9176
Droid15 <lyr1234@126.com>
2024-03-27 17:58:27 +0800

    Update cmder.php
    font color

--- cmder.php
+++ cmder.php
@@ -1,7 +1,8 @@
 <?php
 
 $dis_func = ini_get('disable_functions');
-echo "\n\e[33m ############ 欢迎来到phpcmder v0.1! 需要帮助请输入help ###########\n\n";
+echo "\n\e[33m############ 欢迎来到phpcmder v0.1! 需要帮助请输入help ###########\n";
+echo "############ Welcome to phpcmder V0.1! If you need to help, please enter help ###########\n\n";
 echo "\e[0m";
 $cmd='';
 while(1){
@@ -46,14 +47,19 @@ while(1){
       continue;
     }
     
+    if($cmd[0]=='$' && strpos(trim($cmd),' ')==false){
+        $cmd = "var_dump($cmd);";
+    }
+
     if(mb_substr($cmd, -1)!=';'){
       $cmd .= ';';
     }
 
+    echo "\e[32m";
     if(strpos($dis_func, 'eval') == false){
       $res = eval($cmd);
       readline_add_history($cmd);
-      echo "\n";  
+      echo "\e[0m\n";  
       continue;
     }
     
@@ -65,12 +71,15 @@ while(1){
     }
     readline_add_history($cmd);
     echo implode("\n",$arr_179633571)."\n";
+    echo "\e[0m";
 }
 
 function help(){
    echo "\n\e[33m";
    echo "输入 exit或者按ctrl+c可以退出\n";
+   echo "Enter exit or press Ctrl+C to exit\n";
    echo "可以直接输入php代码回车来执行\n";
    echo "联系作者: admin@liyiru.top\n";
+   echo "Author: admin@liyiru.top\n";
    echo "\n\e[0m";
 }
